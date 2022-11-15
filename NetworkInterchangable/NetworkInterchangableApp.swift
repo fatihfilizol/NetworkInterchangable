ac940f1d997b3fd956a2e3280b49b2a32ad5649f
fatihfilizol <fatihfilizol@gmail.com>
2022-11-15 15:51:25 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Fatih Filizol on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
